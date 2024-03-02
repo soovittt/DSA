b884ea71a6b2f61a274ce1ea03a637e9ee724ee4
Sovit <sovitn@uci.edu>
2024-03-01 21:27:06 -0800

    commit
    commit

---
+++ Graphs/3_find_Center_graph.cpp
@@ -0,0 +1,4 @@
+    int findCenter(vector<vector<int>>& edges) {
+        // first create a adjacency list 
+        // then we can iterate and check if which node has n-1 neighbours thats the center
+    }
