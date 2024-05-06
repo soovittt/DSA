adcfa01b795adf9f195db29a2b530ecbd930a68e
Sovit <sovitn@uci.edu>
2024-05-06 11:57:08 -0700

    commit
    commit

---
+++ Dynamic Programming/11_partition_equal_subset_sum.py
@@ -0,0 +1,59 @@
+#This is the code ninja version of the problem
+def recur(index , target , arr , dp):
+    if(target==0):return True
+    if(index==0):return (arr[0]==target)
+    if(dp[index][target]!=-1):return dp[index][target]
+    not_take = recur(index-1,target,arr,dp)
+    take = False
+    if(arr[index] <= target):
+        take = recur(index-1,target-arr[index],arr,dp)
+    dp[index][target] = take or not_take
+    return dp[index][target]
+
+
+def subsetSumToK(n, k, arr):
+    dp = [[-1 for i in range(k+1)] for j in range(n)]
+    return recur(n-1,k,arr,dp)
+
+
+def canPartition(arr, n):
+    totalSum = sum(arr)
+    if(totalSum%2==1):
+        return False
+    else:
+        k = totalSum // 2
+        return subsetSumToK(n,k,arr)
+    
+
+print(canPartition([2, 3, 3, 3, 4, 5],6))
+
+
+
+
+#This is the Leetcode version of the problem
+class Solution:
+    
+    def recur(self,index , target , arr , dp):
+        if(target==0):return True
+        if(index==0):return (arr[0]==target)
+        if(dp[index][target]!=-1):return dp[index][target]
+        not_take = self.recur(index-1,target,arr,dp)
+        take = False
+        if(arr[index] <= target):
+            take = self.recur(index-1,target-arr[index],arr,dp)
+        dp[index][target] = take or not_take
+        return dp[index][target]
+
+
+    def subsetSumToK(self,n, k, arr):
+        dp = [[-1 for i in range(k+1)] for j in range(n)]
+        return self.recur(n-1,k,arr,dp)
+    
+    def canPartition(self, nums):
+            n = len(nums)
+            totalSum = sum(nums)
+            if(totalSum%2==1):
+                return False
+            else:
+                k = totalSum // 2
+                return self.subsetSumToK(n,k,nums)
---
+++ Dynamic Programming/12_Minimum_subset_difference.py
@@ -0,0 +1,45 @@
+'''
+You are given an integer array nums of 2 * n integers. You need to partition nums into two arrays of length n to minimize the absolute difference of the sums of the arrays. To partition nums, put each element of nums into one of the two arrays.
+'''
+
+#This is the code Ninjas Version of the problem
+
+import sys
+def minSubsetSumDifference(arr, n) -> int:
+    #find the sum of the array to be partitioned
+    target = sum(arr)
+    #construct a dp table 
+    dp = [[0 for i in range(0,target+1)] for j in range(0,n+1)]
+    #set the first column as true 
+    for i in range(0,n+1):
+        dp[i][0] = True
+    #set the top row as true except the dp[0][0]
+    for i in range(1,target+1):
+        dp[0][i] = False
+    # for row in dp:
+    #     print(' '.join(map(str, row)))
+    #then fill up the dp in the bottom up manner 
+    for i in range(1,n+1):
+        for j in range(1,target+1):
+            dp[i][j] = dp[i-1][j]
+            if(arr[i-1] <= j ):
+                dp[i][j] |= dp[i-1][j-arr[i-1]]
+    
+    diff = sys.maxsize
+ 
+    # Find the largest j such that dp[n][j]
+    # is true where j loops from sum/2 t0 0
+    for j in range(target // 2, -1, -1):
+        if dp[n][j] == True:
+            diff = target - (2 * j)
+            break
+        
+    return diff
+
+
+
+    #then we calculate the min difference
+
+
+print(minSubsetSumDifference([8, 6 ,5],3))
+#This is the leetcode version of the problem
